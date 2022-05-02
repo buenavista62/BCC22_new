8bd8d0ff47e7d8fe63ca7739bd4627bdb49c9c6f
Sercan Ates <sercan-ates@outlook.com>
2022-05-02 20:36:37 +0200

    readme

--- contracts/EuroMouse.sol
+++ contracts/EuroMouse.sol
@@ -5,29 +5,29 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Strings.sol";
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 
-contract EuroMouse is ERC721Enumerable, Ownable {
+contract EuroMouse is ERC721, Ownable {
     using Counters for Counters.Counter;
     using SafeMath for uint256;
     uint256 public tokenId;
     uint256 public maxSupply = 101;
     bool public isMintEnabled; //default is false
     string private baseTokenURI;
-    
-    constructor(string memory baseURI) ERC721('BCC EuroMouse - Halloween Edition', 'EUMOUSE') {   
-        setBaseURI(baseURI);     
+
+    constructor(string memory baseURI)
+        ERC721("BCC EuroMouse - Halloween Edition", "EUMOUSE")
+    {
+        setBaseURI(baseURI);
     }
 
     function _baseURI() internal view virtual override returns (string memory) {
-     return baseTokenURI;
+        return baseTokenURI;
     }
 
     function setBaseURI(string memory _baseTokenURI) public onlyOwner {
-     baseTokenURI = _baseTokenURI;
-     
-    }  
+        baseTokenURI = _baseTokenURI;
+    }
 
     function toggleIsMintEnabled() external onlyOwner {
         isMintEnabled = !isMintEnabled;
@@ -38,8 +38,8 @@ contract EuroMouse is ERC721Enumerable, Ownable {
     }
 
     function mint(address _receiver, uint256 tID) public onlyOwner {
-        require(isMintEnabled, 'minting not enabled');
-        require(tokenId <= maxSupply, 'out of range');
-        _safeMint(_receiver, tID);
+        require(isMintEnabled, "minting not enabled");
+        require(tokenId <= maxSupply, "out of range");
+        _mint(_receiver, tID);
     }
 }
