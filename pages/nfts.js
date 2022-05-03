c05c81dffcfdda1de9a796dbbb19f58456e4cad1
Sercan Ates <sercan-ates@outlook.com>
2022-05-03 23:15:19 +0200

    nft carousel

--- pages/nfts.js
+++ pages/nfts.js
@@ -1,21 +1,111 @@
-const photos = [
-  {
-    src: './public/nft/2.png',
-    width: 4,
-    height: 3
-  },
-  {
-    src: './99.png',
-    width: 1,
-    height: 1
-  }
-];
-
+import React, { Component } from 'react';
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Carousel } from 'react-responsive-carousel';
 
 export default function Nfts() {
-  
     return (
-      <Gallery photos={photos} />
+      <Carousel className = "carousel" autoPlay = "true" dynamicHeight = "true" width = "100%" infiniteLoop = "true" transitionTime = "1500" showStatus = "false">
+                  <div>
+                      <img src="/nft/1.png" alt="image1" width="300"/>
+                      <p className="legend">Image 1</p>
+  
+                  </div>
+                  <div>
+                      <img src="/nft/2.png" alt="image2"  width="300"/>
+                      <p className="legend">Image 2</p>
+  
+                  </div>
+                  <div>
+                      <img src="/nft/3.png" alt="image3" width="300"/>
+                      <p className="legend">Image 3</p>
+  
+                  </div>
+                  <div>
+                      <img src="/nft/4.png" alt="image4" width="300"/>
+                      <p className="legend">Image 4</p>
+  
+                  </div>
+                  <div>
+                      <img src="/nft/5.png" alt="image5" width="300"/>
+                      <p className="legend">Image 5</p>
+  
+                  </div>
+                  <div>
+                      <img src="/nft/10.png" alt="image6" width="300"/>
+                      <p className="legend">Image 6</p>
+  
+                  </div>
+                  <div>
+                      <img src="/nft/50.png" alt="image7"  width="300"/>
+                      <p className="legend">Image 7</p>
+  
+                  </div>
+                  <div>
+                      <img src="/nft/99.png" alt="image8" width="300"/>
+                      <p className="legend">Image 8</p>
+  
+                  </div>
+                  <div>
+                      <img src="/nft/72.png" alt="image9" width="300"/>
+                      <p className="legend">Image 9</p>
+  
+                  </div>
+                  <div>
+                      <img src="/nft/62.png" alt="image10" width="300"/>
+                      <p className="legend">Image 10</p>
+  
+                  </div>
+                  <div>
+                      <img src="/nft/42.png" alt="image11" width="300"/>
+                      <p className="legend">Image 11</p>
+  
+                  </div>
+                  <div>
+                      <img src="/nft/100.png" alt="image12" width="300"/>
+                      <p className="legend">Image 12</p>
+  
+                  </div>
+                  <div>
+                      <img src="/nft/31.png" alt="image13"  width="300"/>
+                      <p className="legend">Image 13</p>
+  
+                  </div>
+                  <div>
+                      <img src="/nft/18.png" alt="image14" width="300"/>
+                      <p className="legend">Image 14</p>
+  
+                  </div>
+                  <div>
+                      <img src="/nft/17.png" alt="image15" width="300"/>
+                      <p className="legend">Image 15</p>
+  
+                  </div>
+                  <div>
+                      <img src="/nft/65.png" alt="image16" width="300"/>
+                      <p className="legend">Image 16</p>
+  
+                  </div>
+                  <div>
+                      <img src="/nft/49.png" alt="image17"  width="300"/>
+                      <p className="legend">Image 17</p>
+  
+                  </div>
+                  <div>
+                      <img src="/nft/77.png" alt="image18" width="300"/>
+                      <p className="legend">Image 18</p>
+  
+                  </div>
+                  <div>
+                      <img src="/nft/90.png" alt="image19" width="300"/>
+                      <p className="legend">Image 19</p>
+  
+                  </div>
+                  <div>
+                      <img src="/nft/80.png" alt="image20" width="300"/>
+                      <p className="legend">Image 20</p>
+  
+                  </div>
+              </Carousel>
     )
   }
   
