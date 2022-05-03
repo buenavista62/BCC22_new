52ce6e97ff92c8f335912bd7382afe8b09166867
Sercan Ates <sercan-ates@outlook.com>
2022-05-03 23:59:53 +0200

    pic resize

--- pages/nfts.js
+++ pages/nfts.js
@@ -4,6 +4,7 @@ import { Carousel } from 'react-responsive-carousel';
 
 export default function Nfts() {
     return (
+      <div>
       <Carousel className = "carousel" autoPlay = "true" dynamicHeight = "true" width = "100%" infiniteLoop = "true" transitionTime = "1500" showStatus = "false">
                   <div>
                       <img src="/nft/1.png" alt="image1" width="300"/>
@@ -106,6 +107,7 @@ export default function Nfts() {
   
                   </div>
               </Carousel>
+              </div>
     )
   }
   
