ea42252e09f73d7e873a0105f4887c0068aa8ec9
Sercan Ates <sercan-ates@outlook.com>
2022-05-03 22:47:31 +0200

    png

--- pages/nfts.js
+++ pages/nfts.js
@@ -1,6 +1,22 @@
+const photos = [
+  {
+    src: './public/nft/2.png',
+    width: 4,
+    height: 3
+  },
+  {
+    src: './99.png',
+    width: 1,
+    height: 1
+  }
+];
+
+
 export default function Nfts() {
+  
     return (
-      <div>NFT</div>
+      <Gallery photos={photos} />
     )
   }
   
+
