de26f38c580d1fb41801ddb7c84ff76ca1100b5d
Sercan Ates <sercan-ates@outlook.com>
2022-05-06 21:40:59 +0200

    otomotikmen

--- components/MainMint.js
+++ components/MainMint.js
@@ -9,12 +9,13 @@ import {CheckCircleIcon, MdCheckCircle} from '@chakra-ui/icons'
 import HookForm from "./HookForm";
 
 
+
 const MainMint = () => {
     
     return (
      
         <Flex direction={"column"} justify= "center" align = "center"  paddingBottom = "100px"  mt={[5,10,15]} >
-            <Center fontSize='4xl' mt = {[3,4,5]}  rounded = 'md' boxShadow = '2xl'>Limited EuroMouse Halloween NFTs!</Center>
+            <Center fontSize={['25','40','50']} mt = {[3,4,5]} className = "title" rounded = 'md' >Limited EuroMouse Halloween NFTs!</Center>
             <HookForm/>
             <Text mb = {2} fontSize="3xl" mt={[3,6,9,12]}>
                 Instructions
@@ -29,13 +30,13 @@ const MainMint = () => {
                <ListItem fontSize = "sm"> 
                <ListIcon as={CheckCircleIcon} color='green.500' />Click „submit“ to mint your NFT.</ListItem>
                <ListItem fontSize = "sm"> 
-               <ListIcon as={CheckCircleIcon} color='green.500' />Go to Metamask. Click on the menu on the top left and go to *browser* and type in: <Link href = 'https://testnets.opensea.io'>testnets.opensea.io</Link>.</ListItem>
+               <ListIcon as={CheckCircleIcon} color='green.500' />Go to Metamask. Click on the menu on the top left and go to <i>browser</i> and type in: <Link href = 'https://testnets.opensea.io'>testnets.opensea.io</Link>.</ListItem>
                <ListItem fontSize = "sm"> 
                <ListIcon as={CheckCircleIcon} color='green.500' />Click on the menu on the top right and connect your wallet. </ListItem>
                <ListItem fontSize = "sm"> 
                <ListIcon as={CheckCircleIcon} color='green.500' />Once your wallet is connected click on Account and then on Profile.</ListItem>
                <ListItem fontSize = "sm"> 
-               <ListIcon as={CheckCircleIcon} color='green.500' />Within your profile, swipe to the right to find the section „hidden“. Your NFT will show up here.</ListItem>
+               <ListIcon as={CheckCircleIcon} color='green.500' />Within your profile, swipe to the right to find the section <i>hidden</i>. Your NFT will show up here.</ListItem>
             </List>
         </Flex>
 
--- components/NavBar.js
+++ components/NavBar.js
@@ -46,7 +46,7 @@ export default function NavBar() {
 
   return (
     <>
-      <Box bg={useColorModeValue("gray.100", "gray.900")} px={4}>
+      <Box bg={useColorModeValue("orange.200", "orange.900")} px={4}>
         <Flex h={16} alignItems={"center"} justifyContent={"space-between"}>
           <IconButton
             size={"md"}
